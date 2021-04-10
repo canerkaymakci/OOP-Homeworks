9e1c64940fc51ca5c2c2fcecf69501877da0dcc4
canerkaymakci <canerkaymakci@gmail.com>
2021-04-11 02:12:12 +0300

    Initial commit

---
+++ OOP Work 1.1/Program.cs
@@ -0,0 +1,95 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2020-2021 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: OOP Project 1.1
+**				ÖĞRENCİ ADI............: Caner Kaymakçı
+**				ÖĞRENCİ NUMARASI.......: B191210095
+**                         DERSİN ALINDIĞI GRUP...: Group D
+****************************************************************************/
+
+using System;
+
+namespace OOP_Work_1
+{
+    class Program
+    {
+        static Random random = new Random();  //Used Random class here.
+
+        static void Main(string[] args)
+        {
+            bool[,] castle = new bool[8, 8];     //Created 2 dimensional bool array. Default value is False.
+
+            int coordX, coordY;     //Coord variables.
+            int castlecounter = 0;  //Number of castles
+            int attempt = 0;        //Number of attempts
+
+            //Loop for place all 8 castle. After 8 castle, Loop will be broken.
+            while (true)
+            {
+                coordX = random.Next(0, 8);     //Creates random X coord.
+                coordY = random.Next(0, 8);     //Creates random Y coord.
+                bool tempbool = false;          //Temporary bool for if coords is available for place a castle
+
+                //Checks X and Y coord if it is available for a castle. If yes, Temporary value will stay False.
+                for (int i = 0; i < 8; i++)
+                {
+                    //Checks whole column of an X coord.
+                    if (castle[coordX, i] == true)
+                    {
+                        tempbool = true;
+                        break;
+                    }
+                    //Checks whole line of an Y coord.
+                    if (castle[i, coordY] == true)
+                    {
+                        tempbool = true;
+                        break;
+                    }
+                }
+
+                attempt++;  //Increase attempt value in each loop.
+
+                //If coords are available, program will draw the board at each place time.
+                if (tempbool == false)
+                {
+                    castle[coordX, coordY] = true;    //Coords of castle will be True value.
+                    castlecounter++;                //Number of castle will increase.
+
+                    //Loop for draw a board with castles.
+                    //Loop of each line.
+                    for (int i = 0; i < 8; i++)
+                    {
+                        //Loop of each column.
+                        for (int j = 0; j < 8; j++)
+                        {
+                            Console.Write("[");
+                            //If there is a castle, program will write "C".
+                            if (castle[j, i] == true)
+                            {
+                                Console.Write("C");
+                            }
+                            else
+                            {
+                                Console.Write(" ");
+                            }
+                            Console.Write("]");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("Castle No:" + castlecounter + " was placed in " + attempt + " attempt(s)\n");    //Program will say Castle number and their attempt number on each draw.
+                }
+
+                //If all castles are on board, Main loop will be broken.
+                if (castlecounter == 8)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("\nFinished with " + attempt + " attempts");  //Program will say how many attempts are there at the end of program.
+        }
+    }
+}
---
+++ OOP Work 1.2/Program.cs
@@ -0,0 +1,173 @@
+﻿/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2020-2021 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........: OOP Project 1.2
+**				ÖĞRENCİ ADI............: Caner Kaymakçı
+**				ÖĞRENCİ NUMARASI.......: B191210095
+**                         DERSİN ALINDIĞI GRUP...: Group D
+****************************************************************************/
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace OOP_Work_2
+{
+    class Program
+    {
+        //Method without Substring method.
+        static void indexWithoutSubstr()
+        {
+            Console.WriteLine("Please enter a sentence..: ");
+            string entry = Console.ReadLine().ToLower();    //We takes an entry variable.
+
+            Console.WriteLine("\nPlease enter a word to find (must enter whole word. Can't find just part of a word including punctuations)..: ");
+            string find = Console.ReadLine().ToLower() + ' ';   //We takes a variable to find.
+
+            int index = 0 - find.Length, count = 0; //Index and counter variables.
+
+            //Finds a string in a sentence from the first index.
+            do
+            {
+                index = entry.IndexOf(find, index + find.Length);
+
+                //If there isn't any string to find, program will be stopped.
+                if (index == -1) break;
+
+                count++;
+
+                Console.WriteLine("Word " + find + " is in index " + index + ".");
+
+            } while (index != -1);
+
+            Console.WriteLine("\nWord "+find+" had been used "+count+" times.");
+        }
+
+        //Method with Substring method.
+        static void indexWithSubstr()
+        {
+            Console.WriteLine("Please enter a sentence..: ");
+            string entry=Console.ReadLine().ToLower();      //We takes an entry variable.
+
+            Console.WriteLine("\nPlease enter a word to find..: ");
+            string find = Console.ReadLine().ToLower();     //We takes a variable to find.
+
+            string tempString;      //Used temporary string variable
+            int count = 0;
+
+            //Finds a string in a sentence from the first index.
+            for (int index = 0; index < entry.Length; index++)
+            {
+                //If there is any word that starts with the same letter with find variable, program will check if word is correct. 
+                if (entry[index] == find[0])
+                {
+                    tempString = entry.Substring(index, find.Length);
+                    //If word is correct, we will get index of the word.
+                    if (tempString == find)
+                    {
+                        count++;
+                        Console.WriteLine("Word " + find + " is in index " + index + ".");
+                    }
+                    else continue;
+                }
+                else continue;
+            }
+            Console.WriteLine("\nWord " + find + " had been used " + count + " times.");
+        }
+
+        //Method to find all letters.
+        static void findHowMany()
+        {
+            Console.WriteLine("Enter the sentence..: ");
+
+            string entry = Console.ReadLine().ToLower();    //We takes an entry variable.
+
+            //Used Lists to avoid keeping useless letters.
+            var Letters = new List<char>();
+            var Count = new List<int>();
+
+            int index;
+            string alphabet = "abcçdefgğhıijklmnoöpqrsştuüvwxyz";
+
+            //Checks letters of the whole sentence.
+            foreach (var character in entry)
+            {
+                //If program find a letter for the first time, Letter will be added to list.
+                //If not, Program will increase count of that letter.
+                if (Letters.Contains(character) == false)
+                {
+                    Letters.Add(character);
+                    Count.Add(1);
+                }
+                else
+                {
+                    index = Letters.IndexOf(character);
+                    Count[index]++;
+                }
+            }
+
+            //A loop for write usages in an order.
+            foreach (var character in alphabet)
+            {
+                //If a letter is used, Program will write it with symbols.
+                //If not, Program will write that letter had been used 0 times.
+                if (Letters.Contains(character))
+                {
+                    index = Letters.IndexOf(character);
+                    Console.Write("The letter " + Letters[index] + " had been used " + Count[index] + " times. ");
+                    for (int j = 0; j < Count[index]; j++) Console.Write("*");
+                    Console.WriteLine();
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("The letter " + character + " had been used 0 times.");
+                    continue;
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to program. Pick a choice...\n");
+            string pick;    //Pick variable.
+
+            //We let user pick his/her choice.
+            do
+            {
+                Console.WriteLine("1-) In a string variable, find a string without Substring method.");
+                Console.WriteLine("2-) In a string variable, find a string with Substring method.");
+                Console.WriteLine("3-) In a string variable, find all letters and find how many times be used.");
+                pick = Console.ReadLine();
+
+                //If the pick is incorrect, an error message will be shown.
+                if (pick != "1" && pick != "2" && pick != "3")
+                {
+                    Console.WriteLine("Error. Try again.");
+                    Thread.Sleep(1000);
+                    Console.Clear();
+                }
+            } while (pick!="1"&&pick!="2"&&pick!="3");
+            
+            Console.Clear();
+
+            //Picking a method here.
+            switch (pick)
+            {
+                case "1":
+                    indexWithoutSubstr();
+                    break;
+                case "2":
+                    indexWithSubstr();
+                    break;
+                case "3":
+                    findHowMany();
+                    break;
+            }
+        }
+    }
+}
